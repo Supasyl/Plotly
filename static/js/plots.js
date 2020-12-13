a384652746089c809ac5b70565ba0992d4cbf798
Supasyl <69235823+Supasyl@users.noreply.github.com>
2020-12-13 13:05:41 +0800

    update

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,7 +1,19 @@
 // reading in the data
-const data = 'static/data/samples.json';   
-console.log(data);
+// const data = 'static/data/samples.json';   
+// console.log(data);
 
-// variable for identities
-let subjectID = Object.keys(data)[0];
+// // variable for identities
+// let subjectID = Object.keys(data)[0];
+// console.log(subjectID);
+
+// const dataPromise = d3.json('static/data/samples.json');
+// console.log("Data Promise: ", dataPromise);
+
+function buildPlot() {
+    d3.json('static/data/samples.json').then(function(data) {
+      // Grab values from the data json object to build the plots
+      let subjectID = data.names;
+    });
+};
+buildPlot();
 console.log(subjectID);
