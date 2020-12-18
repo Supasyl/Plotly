0b2feceb8af48268126d8e7ec1c02f1ee42d66a3
Supasyl <69235823+Supasyl@users.noreply.github.com>
2020-12-18 14:07:38 +0800

    Working update

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,70 +1,179 @@
-// empty lists for variables
-let subjectID = [];
-let otu_ids = [];
-let sample_values = [];
-let otu_labels = [];
+// select the dropdown menu input
 let idDropdown = d3.select('#subjectID');
 
-function readJSON() {
+function init() {
 // extract the data from json file
 d3.json("static/data/samples.json").then((data) => {
     // variable for bar chart data
     let samples = (data.samples);
-    console.log(samples);
-    // variable for deomographic info
-    let metadata = (data.metadata);
-    console.log(metadata);
     
-    // run through the bar chart data and add the information in dropdown and variables
+    // run through the data and add the information in dropdown
     samples.forEach((sample) => {
         Object.entries(sample).forEach(([key, value]) => {
-            // list of ID's for the drop down list & add one line for each sample in the dropdown menu
-            if (key === 'id') {subjectID.push(value) && idDropdown.append('option').text(value)};
-            // list of otu ids for the bar chart
-            if (key === 'otu_ids') {otu_ids.push(value)};
-            // list of values for the bar chart
-            if (key === 'sample_values') {sample_values.push(value)};
-            // list of labels for the bar chart
-            if (key === 'otu_labels') {otu_labels.push(value)};
+            // add one line for each sample in the dropdown menu showing the ID value
+            if (key === 'id') {idDropdown.append('option').text(value)};
         });
     }); 
-    chosenID = idDropdown.property('value');
-    console.log(sample_values);
-    console.log(otu_ids); 
-    console.log(metadata[0][0]);
-    // console.log(metadata.id[0]);
 
+    // variable for deomographic info
+    let metadata = (data.metadata);
+  
     // build card
-    d3.select('#card-body').append('p').text(metadata[0][0]);
-    d3.select('#card-body').append('p').text('id: ${metadata.ethnicity}');
-    d3.select('#card-body').append('p').text('id: ${metadata.gender}');
-    d3.select('#card-body').append('p').text('id: ${metadata.age}');
-    d3.select('#card-body').append('p').text('id: ${metadata.location}');
-    d3.select('#card-body').append('p').text('id: ${metadata.bbtype}');
-    d3.select('#card-body').append('p').text('id: ${metadata.wfreq}');
+    Object.entries(metadata[0]).forEach(([key, value]) => {
+        if (key == 'id') {document.getElementById('p1').innerHTML = 'id: ' + value};
+        if (key == 'ethnicity') {document.getElementById('p2').innerHTML = 'ethnicity: ' + value};
+        if (key == 'gender') {document.getElementById('p3').innerHTML = 'gender: ' + value};
+        if (key == 'age') {document.getElementById('p4').innerHTML = 'age: ' + value};
+        if (key == 'location') {document.getElementById('p5').innerHTML = 'location: ' + value};
+        if (key == 'bbtype') {document.getElementById('p6').innerHTML = 'bbtype: ' + value};
+        if (key == 'wfreq') {document.getElementById('p7').innerHTML = 'wfreq: ' + value};
+    });
         
-        
-    // build bar chart
-    let IDotu_ids = otu_ids[0].slice(0, 10);
-    let IDsample_value = sample_values[0].slice(0, 10); 
-    let IDotu_labels = otu_labels[0].slice(0, 10);
-    console.log(IDotu_ids);
-
+    // variables for charts
+    let ID1 = samples[0]
+    ID1.sample_values.sort(function compareFunction(a, b) {
+        return b - a;
+    });
+    let IDotu_ids = ID1.otu_ids;
+    let IDsample_value = ID1.sample_values; 
+    let IDotu_labels = ID1.otu_labels;
     
-    let trace1 = {
+    // build bar chart
+    let trace = {
+        x: IDsample_value.slice(0, 10),
+        y: `OTU ID: ${IDotu_ids.slice(0, 10).toString()}`,
+        text: IDotu_labels.slice(0, 10),
         type: 'bar',
-        x: IDsample_value,
-        y: IDotu_ids,
-        text: IDotu_labels,
         orientation: 'h',
-    }
+        marker: {color: 'burlywood'},        
+    };
     let layout = {
         title: 'Top 10 OTUs',
-        xaxis: IDsample_value,
-        yaxis: 'OTU ID ${IDotu_ids}',
+        xaxis: {'title': 'OTU value'},
+        yaxis: {'tickmode': 'array',
+                'tickvals': ID1.sample_values,
+                'ticktext': `OTU ID: ${IDotu_ids}`}
+    };
+    Plotly.newPlot('barChart', [trace], layout);
+    
+    // create bubble chart
+    let bubble = {
+        x: IDotu_ids,
+        y: IDsample_value,
+        mode: 'markers',
+        marker: {size: IDsample_value,
+                color: IDotu_ids},
+        text: IDotu_labels,
+    };
+    let bubbleLayout = {
+        title: 'All OTU samples for this ID with value for bubble size',
+        xaxis: {'title': 'OTU number'},
+        yaxis: {'title': 'OTU value'},
+    };
+    Plotly.newPlot('bubbleChart', [bubble], bubbleLayout);
+
+    // create gauge chart
+    let gauge = {
+        domain: {row: 0, column: 1},
+        value: metadata[0].wfreq,
+        title: 'Scrubs per week',
+        type: 'indicator',
+        mode: 'gauge+number',
+        text: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+        textinfo: 'text',
+        textposition: 'inside',
+        hoverinfo: 'text',
+        gauge: {
+            axis: {range: [0, 9]},
+            bar: {color: 'darkblue'},
+            steps: [
+                {range: [0, 1], color: 'red'},
+                {range: [1, 2], color: 'darksalmon'},
+                {range: [2, 3], color: 'darksalmon'},
+                {range: [3, 4], color: 'lightgoldenrodyellow'},
+                {range: [4, 5], color: 'lightgoldenrodyellow'},
+                {range: [5, 6], color: 'palegreen'},
+                {range: [6, 7], color: 'palegreen'},
+                {range: [7, 8], color: 'chartreuse'},
+                {range: [8, 9], color: 'chartreuse'},
+            ]
+        }
     }
-    Plotly.newPlot('barChart', trace1, layout);
- 
+    Plotly.newPlot('gaugeChart', [gauge]);
 });  
 };
-readJSON();
+init();
+
+function updatePage() {
+// extract the data from json file
+d3.json("static/data/samples.json").then((data) => {
+    // save the ID number to a variable
+    chosenID = idDropdown.property('value');
+    
+    // variable for deomographic info
+    let metadata = data.metadata;
+    let metadataID = (metadata.filter(record => record.id == chosenID));
+    metadataID = metadataID[0];
+    
+    // build card
+    document.getElementById('p1').innerHTML = `id: ${metadataID.id}`;
+    document.getElementById('p2').innerHTML = `ethnicity: ${metadataID.ethnicity}`;
+    document.getElementById('p3').innerHTML = `gender: ${metadataID.gender}`;
+    document.getElementById('p4').innerHTML = `age: ${metadataID.age}`;
+    document.getElementById('p5').innerHTML = `location: ${metadataID.location}`;
+    document.getElementById('p6').innerHTML = `bbtype: ${metadataID.bbtype}`;
+    document.getElementById('p7').innerHTML = `washing frequency p/w: ${metadataID.wfreq}`;
+        
+    // variables for charts
+    let samples = data.samples;
+    let samplesID = samples.filter(record => record.id == chosenID);
+    // change list (of one item) to a dictionary
+    samplesID = samplesID[0];
+    let x = samplesID.sample_values;
+    let y = samplesID.otu_ids;
+    let text = samplesID.otu_labels;
+
+    // only sort list if there is more than 1 value
+    if (y > 0) {
+        y.sort(function compareFunction(a, b) {
+            return b - a;
+        });
+    };
+
+    // if sample size is under 10 items:
+    if (y.length <= 10) {
+        Plotly.restyle("barChart", "x", [x]);
+        Plotly.restyle("barChart", "y", [`OTU ID: ${y.toString()}`]);
+        Plotly.restyle("barChart", "text", [text]);
+    };
+
+    // if more than 10 items: shorten list & charts
+    if (y.length > 10) {
+        xSlice = x.slice(0, 10);
+        ySlice = y.slice(0, 10);
+        textSlice = text.slice(0, 10);
+        console.log(ySlice);
+        console.log(xSlice);
+        console.log(textSlice);
+
+        Plotly.restyle("barChart", "x", [xSlice]);
+        Plotly.restyle("barChart", "y", [`OTU ID: ${ySlice.toString()}`]);
+        Plotly.restyle("barChart", "text", [textSlice]);
+        Plotly.restyle("barChart", "yaxis.tickvals", [ySlice]);
+        Plotly.restyle("barChart", "yaxis.ticktext", [`OTU ID: ${textSlice}`]);
+    };
+    
+    // Plotly.restyle("bubbleChart", bubbleRestyle);
+    Plotly.restyle("bubbleChart", "x", [samplesID.otu_ids]);
+    Plotly.restyle("bubbleChart", "y", [samplesID.sample_values]);
+    Plotly.restyle("bubbleChart", "text", [text]);
+    Plotly.restyle("bubbleChart", "marker.size", [x]);
+    Plotly.restyle("bubbleChart", "marker.color", [y]);
+
+    // update gauge chart
+    Plotly.restyle("gaugeChart", "value", [metadataID.wfreq]);
+});  
+};
+
+// Add event listener for ID change button
+d3.select("#subjectID").on("change", updatePage);
